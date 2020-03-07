3741fac03c3c019c8198b28dafdcdbb33723b74a
David Wolverton <dwolvert@gmail.com>
2020-03-07 11:38:23 -0500

    4

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import './Timer.css';
 
 class Timer extends Component {
+
+  state = {
+    startTime: null,
+    elapsed: 0
+  }
+
+  handleStart = () => {
+    this.setState({
+      startTime: new Date(),
+      elapsed: 0
+    });
+    this.interval = setInterval(this.timerTick, 1000);
+  }
+
+  handleStop = () => {
+    clearInterval(this.interval); // stop the ticking!
+  }
+
+  timerTick = () => {
+    console.log('tick');
+  }
+
   render() {
+    let startTime;
+    if (this.state.startTime) {
+      startTime = this.state.startTime.toLocaleTimeString();
+    } else {
+      startTime = "Stopped";
+    }
+
     return (
       <div className="Timer">
-        <p><label>Start Time:</label> 3:45:17PM </p>
-        <p><label>Elapsed</label> 0s </p>
+        <p><label>Start Time:</label> {startTime} </p>
+        <p><label>Elapsed</label> {this.state.elapsed}s </p>
         <p>
-          <button>Start</button>
+          <button onClick={this.handleStart}>Start</button>
           <button>Stop</button>
         </p>
       </div>
